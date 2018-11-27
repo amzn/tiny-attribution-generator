4d4824ffd64930057fc29aea271fbb32069864bc
Dan Butvinik <dabutvin@microsoft.com>
2018-11-26 17:18:54 -0800

    extract the package-lock dependencies recursively

--- inputs/packagelock.ts
+++ inputs/packagelock.ts
@@ -7,16 +7,9 @@ import { Package } from '../structure';
 export default class PackageLockSource implements MetadataSource {
   private packageMap: Map<string, Package>;
 
-  constructor(json: string) {
+  constructor(json: string, private readonly excludeDev = true) {
     const content = JSON.parse(json);
-    const dependencies = Object.keys(content.dependencies).map(x => {
-      return {
-        name: x,
-        version: content.dependencies[x].version,
-        text: '_',
-        isDev: content.dependencies[x].dev === true,
-      };
-    });
+    const dependencies = this.extractPackages(content.dependencies, []);
     const pkgkv = dependencies.map<[string, Package]>(p => [
       `${p.name} ${p.version}`,
       p,
@@ -31,4 +24,29 @@ export default class PackageLockSource implements MetadataSource {
   getPackage(id: string): Package | undefined {
     return this.packageMap.get(id);
   }
+
+  extractPackages(deps: dependencyNode, all: Package[]) {
+    if (!deps) return all;
+    Object.keys(deps).map(name => {
+      if (!this.excludeDev || !deps[name].dev) {
+        all.push({
+          name,
+          version: deps[name].version,
+          text: '_',
+        });
+      }
+      this.extractPackages(deps[name].dependencies, all);
+    });
+    return all;
+  }
+}
+
+interface dependencyNode {
+  [name: string]: dependency;
+}
+
+interface dependency {
+  version: string;
+  dev: boolean;
+  dependencies: dependencyNode;
 }
